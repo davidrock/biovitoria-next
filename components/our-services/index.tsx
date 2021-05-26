273a025e5fe564225514137febdd0e5c6c2d0fa4
davidrock <davidrocha@outlook.com>
2021-05-26 00:49:22 -0300

    add more lottie files and improve our-services grid

--- components/our-services/index.tsx
+++ components/our-services/index.tsx
@@ -7,8 +7,8 @@ export function OurServices() {
   });
   return (
     <section>
-      <div className="w-full p-4 grid grid-rows-1 grid-cols-2">
-        <div className="flex justify-center">
+      <div className="w-full p-4 grid grid-rows-1 grid-cols-4">
+        <div className="flex justify-start" style={{ marginTop: "57px" }}>
           <lottie-player
             id="firstLottie"
             ref={ref}
@@ -16,23 +16,21 @@ export function OurServices() {
             loop
             mode="normal"
             src="lotties/pestes.json"
-            style={{ width: "300px", height: "300px" }}
+            style={{ width: "250px", height: "250px" }}
           ></lottie-player>
         </div>
-        <div className="flex justify-start">
+        <div className="flex justify-center col-span-2">
           <div>
-            <h3 className="text-center text-white">
-              Quem é a Biovitoria?
-            </h3>
+            <h3 className="text-center text-white">Quem é a Biovitoria?</h3>
             <h1 className="text-center text-5xl font-padauk font-semibold">
               O que fazemos
             </h1>
 
             <p className="pt-8 text-center text-gray-300">
-              A <strong>BIOVITORIA</strong> oferece os serviços de{" "}
+              A <strong>Biovitoria</strong> oferece os serviços de{" "}
               <strong>dedetização</strong> focados para cada tipo de problema e
               individualizados para cada ramo de atuação. Os serviços de
-              dedetização, realizados pela BIOVITORIA, seguem a linha do uso
+              dedetização, realizados pela Biovitoria, seguem a linha do uso
               racional de praguicídas, manipulação, orientação e controle
               ambiental.
             </p>
@@ -43,6 +41,17 @@ export function OurServices() {
               serviços.
             </p>
           </div>
+        </div>
+        <div className="flex justify-start" style={{ marginTop: "-10px" }}>
+          <lottie-player
+            id="firstLottie"
+            ref={ref}
+            autoplay
+            loop
+            mode="normal"
+            src="lotties/covid-19-protect.json"
+            style={{ width: "500px", height: "500px" }}
+          ></lottie-player>
         </div>
       </div>
     </section>
