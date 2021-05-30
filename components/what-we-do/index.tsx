11bda63fd78535fd68787f897c0e506a43cbdcd6
davidrock <davidrocha@outlook.com>
2021-05-29 21:51:08 -0300

    make 'what-we-do' responsive

--- components/what-we-do/index.tsx
+++ components/what-we-do/index.tsx
@@ -7,76 +7,74 @@ export function WhatWeDo() {
   });
   return (
     <section>
-      <div className="w-full xl:pt-16 grid grid-rows-1 xl:grid-cols-4">
-        <div className="flex justify-start hidden lg:visible">
+      <div className="grid grid-rows-1 md:grid-cols-4">
+        <div className="hidden w-full h-48 justify-center items-center lg:flex lg:mt-24">
           <lottie-player
-            id="plages"
-            ref={ref}
-            autoplay
-            loop
-            mode="normal"
-            src="lotties/pestes.json"
-            style={{ width: "200px", height: "200px" }}
-          ></lottie-player>
-        </div>
-        <div className="flex justify-center col-span-2">
-          <div>
-            <h3 className="text-center text-white">Quem é a Biovitoria?</h3>
-            <h1 className="text-center text-5xl font-padauk font-semibold">
-              O que fazemos
-            </h1>
-
-            <p className="pt-8 text-center text-gray-300">
-              A <strong>Biovitoria</strong> oferece os serviços de{" "}
-              <strong>dedetização</strong> focados para cada tipo de problema e
-              individualizados para cada ramo de atuação. Os serviços de
-              dedetização, realizados pela Biovitoria, seguem a linha do uso
-              racional de praguicídas, manipulação, orientação e controle
-              ambiental.
-            </p>
-
-            <div className="grid justify-center items-center md:gap-28 lg:hidden">
-              <div className="hidden md:visible">
-                <lottie-player
-                  id="plages-2"
-                  ref={ref}
-                  autoplay
-                  loop
-                  mode="normal"
-                  src="lotties/pestes.json"
-                  style={{ width: "120px", height: "120px" }}
-                ></lottie-player>
-              </div>
-              <div className="visible xl:visible">
-                <lottie-player
-                  id="protector-2"
-                  ref={ref}
-                  autoplay
-                  loop
-                  mode="normal"
-                  style={{ width: "250px", height: "250px" }}
-                  src="lotties/covid-19-protect.json"
-                ></lottie-player>
-              </div>
-            </div>
-
-            <p className="xl:pt-4 text-center text-gray-300">
-              Nossos profissionais são capacitados a realizar os serviços com
-              excelência e dar informações técnicas dos serviços e das pragas,
-              orientando os clientes sobre medidas de prevenção e manutenção dos
-              serviços.
-            </p>
-          </div>
-        </div>
-        <div className="flex justify-start hidden lg:visible">
-          <lottie-player
-            id="protector-1"
+            id="outerProtector"
             ref={ref}
             autoplay
             loop
             mode="normal"
             src="lotties/covid-19-protect.json"
-            className="w-4 h-4"
+            style={{ width: "300px", height: "250px" }}
+          ></lottie-player>
+        </div>
+        <div className="w-full md:col-span-4 pb-16 lg:col-span-2">
+          <h3 className="text-center text-white">Quem é a Biovitoria?</h3>
+          <h1 className="text-center text-5xl font-padauk font-semibold">
+            O que fazemos
+          </h1>
+
+          <p className="pt-8 text-center text-gray-300">
+            A <strong>Biovitoria</strong> oferece os serviços de{" "}
+            <strong>dedetização</strong> focados para cada tipo de problema e
+            individualizados para cada ramo de atuação. Os serviços de
+            dedetização, realizados pela Biovitoria, seguem a linha do uso
+            racional de praguicídas, manipulação, orientação e controle
+            ambiental.
+          </p>
+
+          <div className="flex justify-center items-center md:gap-28 lg:hidden flex-row-reverse">
+            <div className="hidden md:flex lg:hidden">
+              <lottie-player
+                id="innerPraga"
+                ref={ref}
+                autoplay
+                loop
+                mode="normal"
+                src="lotties/pestes.json"
+                style={{ width: "130px", height: "130px" }}
+              ></lottie-player>
+            </div>
+            <div className="visible lg:hidden">
+              <lottie-player
+                id="innerProtector"
+                ref={ref}
+                autoplay
+                loop
+                mode="normal"
+                src="lotties/covid-19-protect.json"
+                style={{ width: "250px", height: "250px" }}
+              ></lottie-player>
+            </div>
+          </div>
+
+          <p className="xl:pt-4 text-center text-gray-300">
+            Nossos profissionais são capacitados a realizar os serviços com
+            excelência e dar informações técnicas dos serviços e das pragas,
+            orientando os clientes sobre medidas de prevenção e manutenção dos
+            serviços.
+          </p>
+        </div>
+        <div className="hidden lg:flex w-full h-48 mt-32 justify-center">
+          <lottie-player
+            id="outerPraga"
+            ref={ref}
+            autoplay
+            loop
+            mode="normal"
+            src="lotties/pestes.json"
+            style={{ width: "140px", height: "140px" }}
           ></lottie-player>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,20 +8,20 @@ import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="h-screen">
       <Header />
       <Banner />
       <main>
         <div className="bg-biovitoria text-white">
-          <div className="container mx-auto px-4 py-16">
+          <div className="container mx-auto px-4 py-16 h-full">
             <WhatWeDo />
           </div>
         </div>
-        <div className="bg-gray-50 text-white">
+        {/* <div className="bg-gray-50 text-white">
           <div className="container mx-auto px-4 py-16">
             <OurServices />
           </div>
-        </div>
+        </div> */}
       </main>
 
       <footer className={styles.footer}></footer>
