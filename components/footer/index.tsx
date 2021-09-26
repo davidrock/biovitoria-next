f07e3fd9f197a2833b80a2415b894e95b9318a7d
David Rock <david.martinelli@picpay.com>
2021-09-25 21:24:26 -0300

    add footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,10 +1,14 @@
 export default function Footer() {
   return (
-    <div className='grid grid-flow-col'>
+    <div className='grid grid-flow-col items-center justify-between'>
       <div className=''>
         <p className='text-xl'>Telefones</p>
-        <p>Tel:(27) 3033-8700</p>
-        <p>Cel:(27) 99668-8700</p>
+        <p>
+          <a href='tel:2730338700'>Tel:(27) 3033-8700</a>
+        </p>
+        <p>
+          <a href='tel:27996688700'>Cel:(27) 99668-8700</a>
+        </p>
         <p>Fax:(27) 3534-6270</p>
       </div>
       <div className=''>
@@ -22,16 +26,24 @@ export default function Footer() {
       </div>
       <div className='flex gap-4'>
         <a href='https://www.instagram.com/biovitoria7/' target='blank'>
-          <img src='icons/instagram.svg' width='40' alt='logo' />
+          <img src='icons/instagram.svg' width='40' alt='instagram' />
         </a>
         <a href='https://www.facebook.com/biovitoriaes' target='blank'>
-          <img src='icons/facebook.svg' width='40' alt='logo' />
+          <img src='icons/facebook.svg' width='40' alt='biovitoria' />
         </a>
         <a
           href='https://www.youtube.com/channel/UCXe-ySvFJ4ywi6zFcgGflpw'
           target='blank'>
-          <img src='icons/youtube.svg' width='40' alt='logo' />
+          <img src='icons/youtube.svg' width='40' alt='youtube' />
         </a>
+        <a
+          href='https://wa.me/5527996688700?text=Olá,%20gostaria%20de%20fazer%20um%20orçamento!'
+          target='blank'>
+          <img src='icons/whatsapp.svg' width='40' alt='youtube' />
+        </a>
+      </div>
+      <div className=''>
+        <img src='icons/selo.png' width='130' alt='selo' />
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
+import { useEffect } from 'react';
 
+const tagManagerArgs: TagManagerArgs = {
+  gtmId: 'GTM-TZWNKCH',
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+/*
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZWNKCH');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZWNKCH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+*/
